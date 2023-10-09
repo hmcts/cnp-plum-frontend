f55d8c3c19706fdae6c8e8e4c94b9bc136dd7124
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-09 11:51:53 +0100

    add executable path

--- src/test/config.ts
+++ src/test/config.ts
@@ -6,6 +6,9 @@ process.on('unhandledRejection', reason => {
 export const config = {
   TEST_URL: process.env.TEST_URL || 'http://localhost:1337',
   TestHeadlessBrowser: process.env.TEST_HEADLESS ? process.env.TEST_HEADLESS === 'true' : true,
+  chromeLaunchConfig: {
+    executablePath: "/snap/bin/chromium"
+  },
   TestSlowMo: 250,
   WaitForTimeout: 10000,
 
