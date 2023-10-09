02bf9ae36e14a8ffdc3fc02ecc1d92996f6e1782
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-10-09 11:56:00 +0100

    remove executablePath

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -3,7 +3,6 @@ module.exports = {
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
-  executablePath: '/usr/lib64/chromium-browser/chromium-browser',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
