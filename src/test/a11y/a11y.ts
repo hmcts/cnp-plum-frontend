b33631f925bbfe3287640a9a0ba9020a79882088
Ieuan Byers <ieuan.byers@justice.gov.uk>
2025-02-11 14:01:15 +0000

    Debug pally

--- src/test/a11y/a11y.ts
+++ src/test/a11y/a11y.ts
@@ -50,10 +50,21 @@ async function ensurePageCallWillSucceed(url: string): Promise<void> {
 function runPally(url: string): Promise<Pa11yResult> {
   return pa11y(url, {
     hideElements: '.govuk-footer__licence-logo, .govuk-header__logotype-crown',
-    chromeLaunchConfig: {
-      args: ['--disable-setuid-sandbox'],
+    timeout: 60000,
+    log: {
+      debug: console.log,
+      error: console.error,
+      info: console.info,
     },
-  });
+  })
+    .then(result => {
+      console.log('Pa11y scan completed successfully');
+      return result;
+    })
+    .catch(error => {
+      console.error('Pa11y scan failed:', error);
+      throw error;
+    });
 }
 
 function expectNoErrors(messages: PallyIssue[]): void {
