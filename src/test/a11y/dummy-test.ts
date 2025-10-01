144a008bbcba08a080a671675fd5d8e93b09fdeb
Charles.Luo <charles.luo1@justice.gov.uk>
2025-10-01 17:25:36 +0100

    print something to console

--- src/test/a11y/dummy-test.ts
+++ src/test/a11y/dummy-test.ts
@@ -1,6 +1,8 @@
 /* eslint-disable jest/expect-expect */
 describe('Fake test to work around an issue', () => {
   it('does nothing', async () => {
+    // print something to avoid the test being empty
+    console.log('This is a dummy test to work around an issue with Jest and Cucumber.');
     //see https://github.com/facebook/jest/issues/5783#issuecomment-450626450
   });
 });
