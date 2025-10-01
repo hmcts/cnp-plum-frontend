f25aa41f056ccb6d7e8680f9d79b0d2119aacd2c
Charles.Luo <charles.luo1@justice.gov.uk>
2025-10-01 17:46:20 +0100

    trigger a PR build

--- src/test/a11y/dummy-test.ts
+++ src/test/a11y/dummy-test.ts
@@ -2,7 +2,7 @@
 describe('Fake test to work around an issue', () => {
   it('does nothing', async () => {
     // print something to avoid the test being empty
-    console.log('This is a dummy test to work around an issue with Jest and Cucumber.');
+    console.log('This is a dummy test.');
     //see https://github.com/facebook/jest/issues/5783#issuecomment-450626450
   });
 });
