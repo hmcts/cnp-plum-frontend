ad96b018e23396cd01a26affe87f26bd633d3e80
Charles.Luo <charles.luo1@justice.gov.uk>
2025-10-07 11:20:57 +0100

    minor change

--- src/test/a11y/dummy-test.ts
+++ src/test/a11y/dummy-test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable jest/expect-expect */
 describe('Fake test to work around an issue', () => {
   it('does nothing', async () => {
+    // sth
     //see https://github.com/facebook/jest/issues/5783#issuecomment-450626450
   });
 });
