d0e53184de3f0db724e9831f1a1d90a99145655a
Rees Pozzi <reespozzi@icloud.com>
2026-01-20 10:24:31 +0000

    Updates tar resolution

--- .pnp.cjs
+++ .pnp.cjs
@@ -9883,7 +9883,7 @@ const RAW_RUNTIME_STATE =
           ["minipass-pipeline", "npm:1.2.4"],\
           ["p-map", "npm:7.0.3"],\
           ["ssri", "npm:12.0.0"],\
-          ["tar", "npm:7.5.3"],\
+          ["tar", "npm:7.5.4"],\
           ["unique-filename", "npm:4.0.0"]\
         ],\
         "linkType": "HARD"\
@@ -17512,7 +17512,7 @@ const RAW_RUNTIME_STATE =
           ["nopt", "npm:8.1.0"],\
           ["proc-log", "npm:5.0.0"],\
           ["semver", "npm:7.7.2"],\
-          ["tar", "npm:7.5.3"],\
+          ["tar", "npm:7.5.4"],\
           ["tinyglobby", "npm:0.2.14"],\
           ["which", "npm:5.0.0"]\
         ],\
@@ -20760,14 +20760,14 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["tar", [\
-      ["npm:7.5.3", {\
-        "packageLocation": "./.yarn/cache/tar-npm-7.5.3-fbc2edd71f-106b85ef79.zip/node_modules/tar/",\
+      ["npm:7.5.4", {\
+        "packageLocation": "./.yarn/cache/tar-npm-7.5.4-9413ae778a-c6a2b93a14.zip/node_modules/tar/",\
         "packageDependencies": [\
           ["@isaacs/fs-minipass", "npm:4.0.1"],\
           ["chownr", "npm:3.0.0"],\
           ["minipass", "npm:7.1.2"],\
           ["minizlib", "npm:3.1.0"],\
-          ["tar", "npm:7.5.3"],\
+          ["tar", "npm:7.5.4"],\
           ["yallist", "npm:5.0.0"]\
         ],\
         "linkType": "HARD"\
