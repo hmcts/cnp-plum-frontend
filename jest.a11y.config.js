d63835ee68b1cb891340bbcd60a1cf2d2251632b
Chirag Pareek <chirag.pareek@justice.gov.uk>
2026-05-07 08:47:55 +0100

    fix: mock ioredis in a11y tests to prevent Redis connection timeout

--- jest.a11y.config.js
+++ jest.a11y.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src/test/a11y'],
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
+  testPathIgnorePatterns: ['/node_modules/', '/src/test/a11y/mocks/'],
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
   transform: {
     '^.+\\.ts?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^ioredis$': '<rootDir>/src/test/a11y/mocks/ioredis.ts',
   },
 };
---
+++ src/test/a11y/mocks/ioredis.ts
@@ -0,0 +1,33 @@
+import { EventEmitter } from 'events';
+
+class Redis extends EventEmitter {
+  private store: Map<string, string> = new Map();
+
+  constructor(..._args: unknown[]) {
+    super();
+    process.nextTick(() => {
+      this.emit('connect');
+      this.emit('ready');
+    });
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.store.get(key) ?? null;
+  }
+
+  async set(key: string, value: string): Promise<'OK'> {
+    this.store.set(key, value);
+    return 'OK';
+  }
+
+  async del(...keys: string[]): Promise<number> {
+    keys.forEach(k => this.store.delete(k));
+    return keys.length;
+  }
+
+  async expire(_key: string, _seconds: number): Promise<number> {
+    return 1;
+  }
+}
+
+module.exports = { Redis };
